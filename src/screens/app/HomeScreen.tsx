c22b83444bc71f2ed62b210fef5b4257e4194f5d
Dipo George <dipo@shelftalker.io>
2025-06-18 14:34:09 +0100

    update for nativewind

--- src/screens/app/HomeScreen.tsx
+++ src/screens/app/HomeScreen.tsx
@@ -1,18 +1,10 @@
 import React from 'react';
-import { View, Text, StyleSheet } from 'react-native';
+import { View, Text } from 'react-native';
 
 export default function HomeScreen() {
   return (
-    <View style={styles.container}>
-      <Text>Welcome to the Home Screen!</Text>
+    <View className="flex-1 items-center justify-center">
+      <Text className="text-lg font-semibold">Welcome to the Home Screen!</Text>
     </View>
   );
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
