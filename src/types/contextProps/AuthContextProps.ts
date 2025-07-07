75ae2ba79464bbe4ec325d093b1a4702e7c6fe54
Dipo George <dipo@shelftalker.io>
2025-07-07 07:26:27 +0100

    biometric login

--- src/types/contextProps/AuthContextProps.ts
+++ src/types/contextProps/AuthContextProps.ts
@@ -12,4 +12,5 @@ export interface AuthContextProps {
   ) => Promise<boolean>
   logout: () => Promise<void>
   resetPassword?: (email: string) => Promise<string>
+  authenticateWithBiometrics: () => Promise<boolean>
 }
